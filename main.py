d50ec67c231b5328b92a7c5a936532eeed222b1d
cadzchua <cadidic3h@gmail.com>
2024-05-09 16:45:09 +0800

    feat: bot can now join and leave vc

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-import requests
-import json
+import asyncio
 
 
 
@@ -13,6 +12,7 @@ bot = commands.Bot(command_prefix= "!", intents=intents)
 async def on_ready():
     print("The Bot is ready for use!")
     print("-------------------------")
+    bot.loop.create_task(check_empty_voice_channels())
 
 @bot.event
 async def on_member_join(member):
@@ -45,6 +45,34 @@ async def ping(ctx):
     ping_embed.set_image(url=ctx.guild.icon)
     ping_embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar)
     await ctx.send(embed=ping_embed)
+
+@bot.command()
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not in a voice channel, you must be in a voice channel to run this command.")
+
+@bot.command()
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("I left the voice channel.")
+    else:
+        await ctx.send("I am not in a voice channel.")
+
+async def check_empty_voice_channels():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        for vc in bot.voice_clients:
+            if len(vc.channel.members) == 1:  # Only bot is present
+                await vc.disconnect()
+                ctx = await bot.get_context(vc.channel)
+                await ctx.send("No one is in the voice channel. I left.")
+        await asyncio.sleep(60)  # Check every minute
+
+
 
 
 # Running of Bot
